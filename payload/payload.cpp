dcde93d4b2f00774a1b58f20b381887776036836
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-12-08 20:43:13 +0900

    revert

--- payload/payload.cpp
+++ payload/payload.cpp
@@ -34,7 +34,7 @@ void output(const char *fmt, ...)
     char buf[512];
     va_list va;
     va_start(va, fmt);
-    wvnsprintfA(buf, 512, fmt, va);
+    wvsprintfA(buf, fmt, va);
     va_end(va);
 
     TCHAR szPath[MAX_PATH];
